11f1dbe37ccfe7f45fa32f9fcd2db42140f6f780
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2023-09-29 19:04:36 -0500

    another strategy

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -31,7 +31,7 @@ private:
     size_t max_index = 0;
     for (size_t i = 0; i < data_laser_->ranges.size(); i++)
     {
-      if (data_laser_->ranges[i] > max_distance && data_laser_->ranges[i] < 1.33)
+      if (data_laser_->ranges[i] > max_distance && data_laser_->ranges[i] < 2.3)
       {
         max_distance = data_laser_->ranges[i];
         max_index = i;
@@ -43,11 +43,22 @@ private:
   }
   void timer_callback()
   {
-    //move to the safe distance
-    move_.linear.x = this->linear_x;
-    this->angular_z = direction_ * 0.5;
-    move_.angular.z = this->angular_z;
+    //if the front distance is greater than the safe distance
+    if (data_laser_->ranges[360] > safe_distance_)
+    {
+        //move following the algorithm
+        move_.linear.x = this->linear_x;
+        this->angular_z = direction_ * 0.5;
+        move_.angular.z = this->angular_z;
+    }
+    else
+    {
+        //turn around
+        move_.linear.x = 0;
+        move_.angular.z = 0.5;
+    }
     publisher_->publish(move_);
+    
   }
 
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
@@ -58,6 +69,7 @@ private:
   sensor_msgs::msg::LaserScan::SharedPtr data_laser_;
   float distance_ = 0;
   float direction_ = 0;
+  float safe_distance_ = 0.5;
   rclcpp::TimerBase::SharedPtr timer_;
 };
 
